c32e1d3b35cd0c08a9265b429f0883cb0274848c
Lambda_deep <lambdadeep@gmail.com>
2025-09-14 11:57:44 +0900

    修正

--- app/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { ParentItem as SortableParentItem } from "./ParentItem";
-export { ChildItem as SortableChildItem } from "./ChildItem";
-export { SidebarParentItem as SortableSidebarParentItem } from "./SidebarParentItem";
-export { SidebarChildItem as SortableSidebarChildItem } from "./SidebarChildItem";
